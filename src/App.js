fc45faac09fe8819a0ac2e49d12fc70e805f7cb5
Broderick <zerothesu@gmail.com>
2024-09-25 15:40:44 -0600

    changed icon color, other minor changes to the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css'
-import logo from './YouTube_logo_(2017).png'
 import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <img src={logo}/>
     </div>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import './Header.css'
 import searchIcon from './icons/search-icon.png'
+import micIcon from './icons/microphone-icon.svg'
 
 function Header() {
   return (
@@ -7,6 +8,7 @@ function Header() {
       <form>
         <input type='text' placeholder='Search'/>
         <button className='search-button'><div><img className='search-icon' src={searchIcon}/></div></button>
+        <button className='mic-button'><div><img src={micIcon}/></div></button>
       </form>
     </div>
   )
