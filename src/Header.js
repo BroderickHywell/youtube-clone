15813b9154bbaed52de7b6c9c00a2451407d74c7
Broderick <zerothesu@gmail.com>
2024-09-24 19:11:34 -0600

    Tweaking the header, still in progress

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import './Header.css'
+import searchIcon from './icons/search-icon.png'
 
 function Header() {
   return (
     <div className="Header">
       <form>
         <input type='text' placeholder='Search'/>
+        <button className='search-button'><div><img className='search-icon' src={searchIcon}/></div></button>
       </form>
     </div>
   )
