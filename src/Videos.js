af1306d2e504494dbfb65a02803ba1ba5b5520b8
Broderick <zerothesu@gmail.com>
2024-10-01 16:34:20 -0600

    styled videos section

--- src/Videos.js
+++ src/Videos.js
@@ -1,5 +1,6 @@
 import './Videos.css'
 import thumbnail from './video thumbnails/Screenshot 2022-03-09 165859.png'
+import thumbnail2 from './video thumbnails/Screenshot 2022-03-08 143750.png'
 import pfpIcon from './icons/pfp-icon.svg'
 
 function Videos(){
@@ -17,18 +18,7 @@ function Videos(){
                 </div>
             </div>
             <div className='video'>
-                <img src={thumbnail} alt='video thumbnail' className='thumbnail'/>
-                <div className='video-data'>
-                    <img src={pfpIcon} alt='pfp icon' className='video-pfp-icon'/>
-                    <div className='video-desc'>
-                        <span>video title</span>
-                        <span>creator</span>
-                        <span>views-age</span>
-                    </div>
-                </div>
-            </div>
-            <div className='video'>
-                <img src={thumbnail} alt='video thumbnail' className='thumbnail'/>
+                <img src={thumbnail2} alt='video thumbnail' className='thumbnail'/>
                 <div className='video-data'>
                     <img src={pfpIcon} alt='pfp icon' className='video-pfp-icon'/>
                     <div className='video-desc'>
