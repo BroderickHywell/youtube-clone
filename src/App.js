8d38273ff730f6a997f00d132d2b83ba8d4b94fb
Broderick <zerothesu@gmail.com>
2024-09-30 18:24:48 -0600

    added the categories to the youtube clone and stylized a bit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
+import Categories from './Categories'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
+      <div className='main-content'>
+        <Sidebar />
+        <Categories />
+      </div>
     </div>
   )
 }
---
+++ src/Categories.js
@@ -0,0 +1,18 @@
+import './Categories.css'
+
+function Categories(){
+    return (
+        <div className='Categories'>
+            <span>category</span>
+            <span>category</span>
+            <span>category</span>
+            <span>category</span>
+            <span>category</span>
+            <span>category</span>
+            <span>category</span>
+            <span>category</span>
+        </div>
+    )
+}
+
+export default Categories
