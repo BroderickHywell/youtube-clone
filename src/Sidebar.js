a83ecb2e30fff023fc100058d9769444c8f8c2ac
Broderick <zerothesu@gmail.com>
2024-10-07 19:23:21 -0600

    added sidebar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,25 +1,33 @@
 import './Sidebar.css'
 import tempicon from './icons/pfp-icon.svg'
+import homeIcon from './icons/home-icon.png'
+import shortsIcon from './icons/shorts-icon.png'
+import subscriptionsIcon from './icons/subscriptions-icon.png'
+import yourChannelIcon from './icons/your-channel-icon.png'
+import historyIcon from './icons/history-icon.png'
+import playlistsIcon from './icons/playlists-icon.png'
+import yourVideosIcon from './icons/your-videos-icon.png'
+import watchLaterIcon from './icons/watch-later-icon.png'
+import likedVideosIcon from './icons/liked-videos-icon.png'
+import pewdiepieIcon from './icons/pewdiepie.jpg'
 
 function Sidebar(){
     return (
         <div className="sidebar">
-            <div><img src={tempicon}/>home</div>
-            <div><img src={tempicon}/>Shorts</div>
-            <div><img src={tempicon}/>Subscriptions</div>
+            <div><img src={homeIcon}/>home</div>
+            <div><img src={shortsIcon}/>Shorts</div>
+            <div><img src={subscriptionsIcon}/>Subscriptions</div>
             <hr />
             <div>You</div>
-            <div><img src={tempicon}/>Your channel</div>
-            <div><img src={tempicon}/>History</div>
-            <div><img src={tempicon}/>Playlists</div>
-            <div><img src={tempicon}/>Your videos</div>
-            <div><img src={tempicon}/>Watch later</div>
-            <div><img src={tempicon}/>Liked videos</div>
+            <div><img src={yourChannelIcon}/>Your channel</div>
+            <div><img src={historyIcon}/>History</div>
+            <div><img src={playlistsIcon}/>Playlists</div>
+            <div><img src={yourVideosIcon}/>Your videos</div>
+            <div><img src={watchLaterIcon}/>Watch later</div>
+            <div><img src={likedVideosIcon}/>Liked videos</div>
             <hr />
             <div>Subscriptions</div>
-            <div><img src={tempicon}/>channel</div>
-            <div><img src={tempicon}/>channel</div>
-            <div><img src={tempicon}/>channel</div>
+            <div><img src={pewdiepieIcon} className='pewdiepie-pfp'/>pewdiepie</div>
         </div>
     )
 }
